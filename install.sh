e1d3f337db5ad4b8e3e983e8abb90d9389642dc5
krish3402 <40502996+krish3402@users.noreply.github.com>
2023-01-05 02:03:31 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+apt update
+apt-get install build-essential
+apt install curl git -y
+echo -e '\n' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew doctor
+brew --help
+brew install cloudfoundry/tap/bosh-cli
+bosh -v
+bosh --help
